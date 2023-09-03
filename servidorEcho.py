a557cdd99f08df8e8a7492de9c1a604fac8cb2c6
Ryan Pacheco <102881907+ryanvpacheco@users.noreply.github.com>
2023-09-03 10:17:26 -0300

    Add files via upload

---
+++ servidorEcho.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+def handle_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024).decode("utf-8")
+            if not data:
+                break
+            
+            parts = data.split(" ")
+            command = parts[0].lower()
+            
+            if command == "echo":
+                message = " ".join(parts[1:])
+                client_socket.send(message.encode("utf-8"))
+            elif command == "quit":
+                client_socket.send(b"Goodbye!")
+                break  # Encerra o loop quando o comando "quit" é recebido
+            else:
+                client_socket.send(b"Unknown command")
+    finally:
+        client_socket.close()
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", 12345))
+    server.listen(5)
+    print("Server listening on port 12345")
+    
+    running = True
+    
+    try:
+        while running:
+            client_socket, addr = server.accept()
+            print(f"Accepted connection from {addr}")
+            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+            client_thread.start()
+    except KeyboardInterrupt:
+        running = False
+        server.close()
+        print("Server stopped")
+
+if __name__ == "__main__":
+    main()
